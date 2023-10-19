bf6243c6a187575a8a261a1e75fb3572952cd8ee
salfo22 <bougmasalfo81@gmail.com>
2023-10-19 09:51:35 +0100

    salfo/solution classe

--- Armes.py
+++ Armes.py
@@ -1,1 +1,14 @@
 
+class Arme:
+    def __init__(self, nom, portee, degats):
+        self.nom = nom  # Nom de l'arme (par exemple, "Canon principal")
+        self.portee = portee  # Portée de l'arme en mètres
+        self.degats = degats  # Dégâts infligés par l'arme
+
+    def tirer(self, cible):
+        # Méthode pour effectuer un tir avec l'arme
+        pass
+
+    def recharger(self):
+        # Méthode pour recharger l'arme
+        pass
